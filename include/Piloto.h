bc3c71c3e353804040c33796a9b6994784e0b576
Ana Carolina Costa Pereira <carolina.cst.prr@gmail.com>
2025-01-26 10:02:00 -0300

    Mudanças nas opções predefinidas de pilotos

--- include/Piloto.h
+++ include/Piloto.h
@@ -8,10 +8,22 @@ class Piloto: public: Carta{
     std:: string efeito_piloto;
     Unidade unidade_tripulada;
     public:
+    Piloto(std::string _efeito_piloto, Unidade Unidade, std::string _Desc, std::string _Tipo, std::string _Nome, int _Custo, int _ID );
+    ~Piloto();
     void fornece_efeito() override;
     
-
 };
+
+namespace PilotosPredefinidos{
+    extern const Piloto AmuroRay;
+    extern const Piloto KayShiden;
+    extern const Piloto HayatoKobayashi;
+    extern const Piloto PilotoFederacao;
+    extern const Piloto CharAznable;
+    extern const Piloto DozleZabi;
+    extern const Piloto RambaRal;
+    extern const Piloto PilotoZeon;
+}
 
 
 #endif
