6c63112ed8b0cd9d32aa280c22f2e2782ac723cf
PatrickSN <86534143+PatrickSN@users.noreply.github.com>
2025-01-28 10:28:46 -0300

    inicializacao baralho

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,51 @@ using namespace PilotosPredefinidos;
 using namespace InstanteneoPredefinidos;
 
 
+void inicializaBaralho(Baralho& baralho) {
+    // Adiciona unidades ao baralho
+    baralho.adiciona_carta(new Unidade(RX782Gundam));
+    baralho.adiciona_carta(new Unidade(RX75Guntank));
+    baralho.adiciona_carta(new Unidade(Guncannon));
+    baralho.adiciona_carta(new Unidade(Ball));
+    baralho.adiciona_carta(new Unidade(GM));
+    baralho.adiciona_carta(new Unidade(Zeong));
+    baralho.adiciona_carta(new Unidade(BigZam));
+    baralho.adiciona_carta(new Unidade(Gelgoog));
+    baralho.adiciona_carta(new Unidade(ZGok));
+    baralho.adiciona_carta(new Unidade(Gouf));
+    baralho.adiciona_carta(new Unidade(Zaku));
 
+    // Adiciona equipamentos ao baralho
+    baralho.adiciona_carta(new Equipamento(GundamHammer));
+    baralho.adiciona_carta(new Equipamento(ArmaduraChobam));
+    baralho.adiciona_carta(new Equipamento(UpgradeGundam));
+    baralho.adiciona_carta(new Equipamento(ReatorPartículasMinovsky));
+
+    // Adiciona pilotos ao baralho
+    baralho.adiciona_carta(new Piloto(AmuroRay));
+    baralho.adiciona_carta(new Piloto(KayShiden));
+    baralho.adiciona_carta(new Piloto(HayatoKobayashi));
+    baralho.adiciona_carta(new Piloto(PilotoFederacao));
+    baralho.adiciona_carta(new Piloto(CharAznable));
+    baralho.adiciona_carta(new Piloto(DozleZabi));
+    baralho.adiciona_carta(new Piloto(RambaRal));
+    baralho.adiciona_carta(new Piloto(PilotoZeon));
+
+    // Adiciona instantâneos ao baralho
+    baralho.adiciona_carta(new Instantaneo(SistemaRaioSolar));
+    baralho.adiciona_carta(new Instantaneo(ReforcosDaFederacao));
+    baralho.adiciona_carta(new Instantaneo(OperacaoBritanica));
+    baralho.adiciona_carta(new Instantaneo(NovaInvestida));
+    baralho.adiciona_carta(new Instantaneo(INEUZaku));
+    baralho.adiciona_carta(new Instantaneo(DesengajarCombate));
+    baralho.adiciona_carta(new Instantaneo(CINADSN));
+    baralho.adiciona_carta(new Instantaneo(ADCV));
+}
 
 int main() {
     // Cria um baralho com algumas cartas
     Baralho baralho;
+    inicializaBaralho(baralho);
 
     // Cria dois jogadores com o baralho
     std::string j1, j2;
