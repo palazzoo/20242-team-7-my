e81b49c371b59ef83e856f0055f1a10af8d86b8f
Ana Carolina Costa Pereira <carolina.cst.prr@gmail.com>
2025-01-28 09:37:28 -0300

    Método ativaEvasao

--- src/Unidade.cpp
+++ src/Unidade.cpp
@@ -1,4 +1,4 @@
-#include "../include/Unidade.h"
+#include "Unidade.h"
 
 int Unidade::getHp(){
     return Hp;
@@ -13,7 +13,7 @@ int Unidade::getDef(){
 }
 
 Unidade::Unidade(int _Atk, int _Def, int _Hp, int _Custo, std::string _Desc, std::string _Tipo, std::string _Nome, bool _Equip, int _ID, bool _guarda)
-    : Carta(_Desc, _Tipo, _Nome, _Custo, _ID), Atk(_Atk), Def(_Def), Hp(_Hp), Equip(_Equip), Guarda(_guarda)  {}
+    : Carta(_Desc, _Tipo, _Nome, _Custo, _ID), Atk(_Atk), Def(_Def), Hp(_Hp), Equip(_Equip), Guarda(_guarda), Evasao(false)  {}
 
 Unidade::Unidade(int _Atk, int _Def, int _Hp, int _Custo, std::string _Desc, std::string _Tipo, std::string _Nome, bool _Equip, int _ID, bool _guarda, int _calor_produzido)
     : Carta(_Desc, _Tipo, _Nome, _Custo, _ID),
@@ -55,6 +55,14 @@ void Unidade::setAtk(int _Atk) {
 
 void Unidade::ativa_guarda(){
     Guarda = true;
+}
+
+bool Unidade::getEvasao(){
+    return Evasao;
+}
+
+void Unidade::ativaEvasao(){
+    Evasao=true;
 }
 
 
