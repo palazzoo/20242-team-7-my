00fad83c995b82d71de392e7ad933c9c5d23a51b
Ana Carolina Costa Pereira <carolina.cst.prr@gmail.com>
2025-01-28 10:16:53 -0300

    Correção nos nomes de alguns objetos

--- src/InstantaneoPredefinidas.cpp
+++ src/InstantaneoPredefinidas.cpp
@@ -1,10 +1,9 @@
 #include "../include/Instantaneo.h"
-#
 
 namespace InstanteneoPredefinidos{
     const Instantaneo SistemaRaioSolar = Instantaneo("Desc",  "Instantaneo",  "Sistema Raio Solar",  6, 21);
-    const Instantaneo ReforcosDaFederacao = Instantaneo("Desc",  "Instantaneo",  "Reforços da Federaçao",  4, 22);
-    const Instantaneo OperacaoBritanica = Instantaneo("Desc",  "Instantaneo",  "Operaçao Britânica",  9, 23);
+    const Instantaneo ReforcosDaFederacao = Instantaneo("Desc",  "Instantaneo",  "Reforços da Federacao",  4, 22);
+    const Instantaneo OperacaoBritanica = Instantaneo("Desc",  "Instantaneo",  "Operaçao Britanica",  9, 23);
     const Instantaneo NovaInvestida = Instantaneo("Desc",  "Instantaneo",  "Nova Investida",  3, 24);
     const Instantaneo INEUZaku = Instantaneo("Desc",  "Instantaneo",  "Isso nao e um Zaku, garoto",  2, 25);
     const Instantaneo DesengajarCombate = Instantaneo("Desc",  "Instantaneo",  "Desengajar Combate", 1, 26);
